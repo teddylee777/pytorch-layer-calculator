da66715eb252d43660a13057d2eaade79e8af096
teddylee777 <teddylee777@gmail.com>
2022-09-03 04:44:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
+import math
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
+
 import streamlit as st
 import streamlit.components.v1 as components
-import math
 
 
 st.set_page_config(page_title='pytorch calculator', layout='centered')
-
-components.html('''
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-X4423L75Z6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-X4423L75Z6');
-</script>
-''')
-
 
 ERR_MSG_NUMBER = 'Only NUMBERs are allowed as an input and it cannot be omitted.'
 
@@ -203,3 +195,51 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
+
+def inject_ga():
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+    # web application's id is. You will find this in your Google Analytics account
+    
+    GA_JS = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X4423L75Z6"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-X4423L75Z6');
+</script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+inject_ga()
+
+components.html('''
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X4423L75Z6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-X4423L75Z6');
+</script>
+</head>
+''', width=0, height=0)
