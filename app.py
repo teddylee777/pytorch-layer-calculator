163a8b9e0573daf7f3c7fd849aa332da154412d8
teddylee777 <teddylee777@gmail.com>
2022-09-03 03:31:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -181,17 +181,10 @@ st.write('''
 ----
 ''')
 
-
-
-# conv2d = st.text_input(label="User Name", value="default value")
-# radio_gender = st.radio(label="Gender", options=["Male", "Female"])
-# check_1 = st.checkbox(label="agree", value=False)
-# memo = st.text_area(label="memo", value="")
-
-# if st.button("Confirm"):
-#     con = st.container()
-#     con.caption("Result")
-#     con.write(f"User Name is {str(input_user_name)}")
-#     con.write(str(radio_gender))
-#     con.write(f"agree : {check_1}")
-#     con.write(f"memo : {str(memo)}")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
