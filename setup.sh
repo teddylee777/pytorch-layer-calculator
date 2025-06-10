2672a5b7fdc890f8539ad36adbe576f3e13de8fe
Teddy Lee <teddylee777@gmail.com>
2025-06-11 00:41:40 +0900

    Add Streamlit theme configuration
    - Introduced a new configuration file for Streamlit to define the app's theme settings.
    - Updated setup.sh to create the configuration file with primary and background colors, text color, font, and theme base.
    - Enhanced visual consistency and user experience with a light theme and custom color palette.

--- setup.sh
+++ setup.sh
@@ -5,4 +5,12 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
+[theme]\n\
+primaryColor = \"#ff6b6b\"\n\
+backgroundColor = \"#FFFFFF\"\n\
+secondaryBackgroundColor = \"#f0f2f6\"\n\
+textColor = \"#262730\"\n\
+font = \"sans serif\"\n\
+base = \"light\"\n\
 " > ~/.streamlit/config.toml
